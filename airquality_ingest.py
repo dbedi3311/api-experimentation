379fdd753032e4a2d04e615040e4daf4bf65fa2a
Divjot Bedi <dbedi3311@gmail.com>
2022-06-02 13:20:58 -0400

    making simple request

--- airquality_ingest.py
+++ airquality_ingest.py
@@ -1,2 +1,20 @@
 
+import time
+import requests
+
+#Data is being queried from OpenAQ (open air-quality data)
+URL_MEASUREMENTS = "https://api.openaq.org/v2/measurements"
+
+def make_request(url, params):
+    r = requests.get(url, params=params)
+    # If no valid response from the api print error and retry
+    while not r:
+        print("API did not return 200 response")
+        time.sleep(5)
+        r.requests.get(url, params=params)
+    return r
+
+payload = {}
+res = make_request(URL_MEASUREMENTS, payload)
+print(res.json())
 
