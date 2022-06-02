1656f4d27abe9f62b644869ec66a3f3bf5e3dac3
Divjot Bedi <dbedi3311@gmail.com>
2022-06-02 15:48:38 -0400

    determined JSON schema from endpoint

--- airquality_ingest.py
+++ airquality_ingest.py
@@ -1,9 +1,12 @@
 
 import time
 import requests
+from pprintpp import pprint
+from genson import SchemaBuilder
 
 #Data is being queried from OpenAQ (open air-quality data)
 URL_MEASUREMENTS = "https://api.openaq.org/v2/measurements"
+URL_LOCATIONS = "https://api.openaq.org/v2/locations"
 
 def make_request(url, params):
     r = requests.get(url, params=params)
@@ -15,6 +18,31 @@ def make_request(url, params):
     return r
 
 payload = {}
+#retrieving all data for the measurements
 res = make_request(URL_MEASUREMENTS, payload)
-print(res.json())
+#print(res.json())
+
+#issue here is that it is hard to read the structure of the data itself, so how about we try prettyprint
+
+#retrieving all data for locations
+res = make_request(URL_LOCATIONS, {})
+json_res = res.json()
+#print(json_res)
+#pprint(json_res)
+
+#prettyprint helped us decipher the complicated nested structure a tad bit, but since the output is so enormous,
+#it is still hard for us to understand what is happening. What we want is referred to as the JSON schema (a shallow structure)
+
+#we use genson to solve this issue
+
+builder = SchemaBuilder()
+builder.add_object(json_res)
+pprint(builder.to_schema())
+
+# retrieving all the country locations.
+
+
+
+
+
 
